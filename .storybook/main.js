de1f7e483c35777329bd64998f2b632f5e131ed8
Byron 2016 <bgva2005@yahoo.com>
2021-11-21 18:03:22 -0500

    001_css_cssmodules Borrar carpetas creadas por create-react-app

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ["../atomDesign/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-css-modules-preset",
+  ],
+};
---
+++ .storybook/preview.js
@@ -0,0 +1,12 @@
+import "../styles/main.css";
+import "../styles/tokens.css";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
