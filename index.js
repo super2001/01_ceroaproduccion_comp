8a75d0423de4f0a4094e5b6fc3bad445fee1a863
Byron 2016 <bgva2005@yahoo.com>
2021-11-21 18:05:09 -0500

    001_css_cssmodules Exportar componentes en index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// Layout
+export { default as Spacer } from "./atomDesign/layout/Spacer";
+
+// Atoms
+export { default as NButton } from "./atomDesign/atoms/Button";
+export { default as Heading } from "./atomDesign/atoms/Heading";
+export { default as Icon } from "./atomDesign/atoms/Icons";
+export { default as Input } from "./atomDesign/atoms/Input";
+export { default as Picture } from "./atomDesign/atoms/Picture";
+
+// Molecules
+export { default as ButtonIcon } from "./atomDesign/molecules/ButtonIcon";
+export { default as Dropdown } from "./atomDesign/molecules/Dropdown";
